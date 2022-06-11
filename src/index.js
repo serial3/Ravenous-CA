78f3ff6d4b5abbab0f5e32cca9ba4bb022b147ca
serial3 <diogo_R_Leal@hotmail.com>
2022-06-11 15:30:33 +0100

    searchbar not working

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/app/app';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    App / >
+    <
+    /React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://github.com/facebook/create-react-app/blob/main/packages/cra-template/template/README.md
+serviceWorker.unregister();
